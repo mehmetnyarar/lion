670161d8c8a110033185565a3eccd90730a02404
Mehmet Niyazi Yarar <mehmetnyarar@gmail.com>
2020-11-27 12:46:26 +0600

    test: fix a11y testing

--- __tests__/components/language.spec.tsx
+++ __tests__/components/language.spec.tsx
@@ -1,3 +1,4 @@
+import { axe } from 'jest-axe'
 import React from 'react'
 import { fireEvent, render } from 'test/utils'
 import { LanguageSelector } from '~/components/language'
@@ -7,11 +8,11 @@ jest.spyOn(console, 'info')
 
 describe('components/language', () => {
   it('should render', async () => {
-    const { getByLabelText } = render(<LanguageSelector />)
+    const { container, getByLabelText } = render(<LanguageSelector />)
 
     // Check a11y
-    // const a11y = await axe(container)
-    // expect(a11y).toHaveNoViolations()
+    const a11y = await axe(container)
+    expect(a11y).toHaveNoViolations()
 
     // Check the default language
     const select = getByLabelText('language') as HTMLSelectElement
